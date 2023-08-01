1c56797c265c11bf0cb4115ba3b252bc9d5a9afc
lynnagidza <lynnagidza@gmail.com>
2023-08-01 17:58:36 +0300

    tasks 1 - 3

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+""" Get locale from request """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """ Babel Config Class """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """ Get locale from request """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/", methods=["GET"], strict_slashes=False)
+def index():
+    """ GET /
+    Return:
+      - 2-index.html
+    """
+    return render_template("2-index.html")
